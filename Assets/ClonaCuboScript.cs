3f5f64ca9dcd8af879e7e4493e3de4b757c992be
Frotolin <mathifogo@gmail.com>
2025-05-15 10:39:27 -0300

    terminado FPS

---
+++ Assets/ClonaCuboScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClonaCuboScript : MonoBehaviour
+{
+    public GameObject cuboPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClonarCubo();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ClonarCubo()
+    {
+        Instantiate(cuboPrefab);
+
+    }
+}
---
+++ Assets/DeathRespawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathRespawn : MonoBehaviour
+{
+    public float deathHeight = -11;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y< deathHeight)
+        {
+            Debug.Log("respawn");
+            SceneManager.LoadScene("FPSParkour");
+
+        }
+    }
+}
